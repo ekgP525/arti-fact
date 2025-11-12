62f289453b863eb2a8d3fc62be5ae28aac59711b
y.tnwjd <ysjsw@ajou.ac.kr>
2025-11-12 17:34:21 +0900

    git init

---
+++ src/main/java/ajou/artifact/arti_fact/ArtiFactApplication.java
@@ -0,0 +1,13 @@
+package ajou.artifact.arti_fact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtiFactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtiFactApplication.class, args);
+	}
+
+}
