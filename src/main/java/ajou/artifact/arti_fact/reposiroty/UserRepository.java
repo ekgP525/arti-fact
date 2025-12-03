36a6aac41e6cc3b47e62f089de4d3d10a6aaa190
y.tnwjd <ysjsw@ajou.ac.kr>
2025-12-03 19:24:07 +0900

    fix: integrate userServices

--- src/main/java/ajou/artifact/arti_fact/reposiroty/UserRepository.java
+++ src/main/java/ajou/artifact/arti_fact/reposiroty/UserRepository.java
@@ -1,7 +1,7 @@
 package ajou.artifact.arti_fact.reposiroty;
 
-import ajou.artifact.arti_fact.entity.Users;
+import ajou.artifact.arti_fact.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
